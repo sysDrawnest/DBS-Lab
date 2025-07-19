c297585f665dd2bb655fd2b38950c67acb7dceaa
Sai Yangyadatta Sethy <118187096+sysDrawnest@users.noreply.github.com>
2025-07-19 23:48:06 +0530

    Create Intro.sql

---
+++ Intro.sql
@@ -0,0 +1,16 @@
+-- File: company_setup.sql
+-- Purpose: Creates a new database named CompanyUU and displays its tables
+-- Author: [Your Name]
+-- Date: 2025-07-19
+
+-- Display all available databases
+SHOW DATABASES;
+
+-- Create a new database named CompanyUU
+CREATE DATABASE CompanyUU;
+
+-- Switch to the CompanyUU database
+USE CompanyUU;
+
+-- List all tables in the CompanyUU database
+SHOW TABLES;
