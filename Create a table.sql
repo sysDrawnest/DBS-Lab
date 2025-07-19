b4f1fff2745a572b067cf89b86eccb09b2440ab7
Sai Yangyadatta Sethy <118187096+sysDrawnest@users.noreply.github.com>
2025-07-19 23:50:31 +0530

    Create Create a table.sql

---
+++ Create a table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employee (
+    Emp_no INTEGER PRIMARY KEY,
+    Name VARCHAR(25),
+ Address VARCHAR(20),
+    Mobile VARCHAR(10)
+ );
+
+INSERT INTO Employee 
+VALUES (10101, 'Rahul', 'BBSR', '9876543210');
