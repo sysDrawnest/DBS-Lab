f82a342a588e1e1807d206f7e26e438adac09be1
Sai Yangyadatta Sethy <118187096+sysDrawnest@users.noreply.github.com>
2025-07-19 23:56:13 +0530

    Update insert values to a table.sql

--- insert values to a table.sql
+++ insert values to a table.sql
@@ -9,3 +9,21 @@ VALUES (10101, 'Rahul', 'BBSR', '9876543210');
 (1007, 'Rakesh Behera', 'Angul', '9786543210'),
 (1008, 'Kiran Panda', 'Baripada', '9090909090'),
  (1009, 'Laxmi Jena', 'Puri', '9321654987');
+
+```sql
+-- Output of the query: SELECT * FROM employee;
+-- +--------+---------------+-------------+------------+
+-- | Emp_no | Name          | Address     | Mobile     |
+-- +--------+---------------+-------------+------------+
+-- |   1001 | Rajesh Kumar  | Cuttack     | 9876543210 |
+-- |   1002 | Priya Das     | Bhubaneswar | 9123456780 |
+-- |   1003 | Amit Nayak    | Sambalpur   | 9988776655 |
+-- |   1004 | Sneha Mohanty | Rourkela    | 9012345678 |
+-- |   1005 | Manoj Sahu    | Berhampur   | 9345678901 |
+-- |   1006 | Swati Patnaik | Balasore    | 9865321470 |
+-- |   1007 | Rakesh Behera | Angul       | 9786543210 |
+-- |   1008 | Kiran Panda   | Baripada    | 9090909090 |
+-- |   1009 | Laxmi Jena    | Puri        | 9321654987 |
+-- |  10101 | Rahul         | BBSR        | 9876543210 |
+-- +--------+---------------+-------------+------------+
+-- 10 rows in set (0.00 sec)
